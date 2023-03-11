3d845051fbd20eddb7bd3775e5a4fe500399f3b0
Richard Khillah <rkhillah@gmail.com>
2023-03-10 22:02:02 -0800

    Message implemented

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ import asyncio
 import collections
 import logging
 import sys
+import time
 
 from typing import List, Tuple
 
@@ -47,6 +48,72 @@ def parse(args: List[str]) -> Tuple[str, int]:
 def parse_iamat():
     pass
 
+TYPE = {
+    'IAMAT': None,
+    'WHATISAT': None,
+    # 'IAM': None,
+}
+
+class Message:
+    def __init__(self, message, received_time=None):
+        self.type=None
+        self.skew=None
+        self.addr=None
+        self.lon=None
+        self.lat=None
+        self.radius=None
+        self.pagination=None
+        self.client_time=None
+        self._message=message
+        if message.startswith(('IAMAT', 'WHATISAT')):
+            m = message.split()
+            type=m[0]
+            if type == 'IAMAT' and len(m) == 4:
+                # IAMAT kiwi.cs.ucla.edu +34.068930-118.445127 1621464827.959498503
+                self.type = 'IAMAT'
+                self.addr = m[1]
+                coords = self.crude_coord_split(m[2])
+                self.lat = (coords[0], coords[1]) # ['+/-', 'floatstring']
+                self.lon = (coords[2], coords[3]) # ['+/-', 'floatstring']
+                self.client_time = m[3]
+                self.skew = received_time - float(self.client_time)
+
+            elif type == 'WHATISAT' and len(m) == 4:
+                #TODO: Implement
+                pass
+            
+    def _body(self):
+        b = self.addr
+        c = "".join([*self.lon, *self.lat]) if self.type == 'IAMAT' else self.radius
+        d = self.client_time if self.type == 'IAMAT' else self.pagination
+        return f'{b} {c} {d}'
+    
+    def __str__(self):
+        a = self.type
+        b = self._body()
+        return f"{a} {b}"
+    
+    def response(self, at, payload=None):
+        if self.type not in ('IAMAT', 'WHATISAT'):
+            return f"? {self._message}"
+        return f"AT {at} {self.skew} {self._body()}"
+
+    def crude_coord_split(self, coordstr):
+        # s = '+34.068930-118.445127'
+        ret = []
+        temp=""
+        for i in coordstr:
+            if i in ['+', '-']:
+                if temp != "":
+                    ret.append(temp)
+                ret.append(i)
+                temp=""
+                continue
+            temp += i
+        ret.append(temp)
+        # FIX ME Ensure coordinates have the above form
+        return ret
+
 async def handle_echo(reader, writer):
     # Read info from sender
     data = await reader.read(100) # Want this to be as many as needed
@@ -56,11 +123,13 @@ async def handle_echo(reader, writer):
     logging.info(f"Received {message} from {addr}")
     print(f"Received {message!r} from {addr!r}")
 
+    resp = Message(message, time.time()).response('Bailey', None)
+
     # Reply to sender
-    print(f"Send: {message!r}")
-    writer.write(data)
+    print(f"Send: {resp!r}")
+    writer.write(resp.encode())
     await writer.drain()
-    logging.info(f"Sent {message} to {addr}")
+    logging.info(f"Sent {resp} to {addr}")
 
     print("Close the connection")
     writer.close()
