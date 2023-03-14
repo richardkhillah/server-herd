df6f316fa5695ef3537d30486da20f10a68763b2
Richard Khillah <rkhillah@gmail.com>
2023-03-14 14:23:00 -0700

    cleanup and read if not eof

--- server.py
+++ server.py
@@ -14,7 +14,6 @@ from record import Record, Position
 
 logger = logging.getLogger(__name__)
 
-log_to_console = True
 use_dummy_api = True
 use_my_herd = True
 
@@ -35,18 +34,6 @@ def init_logger(filename):
 
     logger.addHandler(c_handler)
     logger.addHandler(f_handler)
-
-    # if log_to_console:
-    #     c_handler = logging.StreamHandler()
-
-    #     c_handler.setLevel(logging.DEBUG)
-
-    #     c_format = logging.Formatter('%(message)s')
-
-    #     c_handler.setFormatter(c_format)
-
-    #     logger.addHandler(c_handler)
-
 
 my_herd = {
     'Bailey': 17800,
@@ -160,144 +147,126 @@ async def do_stuff(reader, writer, request):
 
 async def handle_echo(reader, writer):
     # Read info from sender
-    data = await reader.read() # Want this to be as many as needed
+    while not reader.at_eof():
+        data = await reader.read() # Want this to be as many as needed
     
-    message = data.decode()
-    # addr = writer.get_extra_info('peername')
-    # logger.info(f"Received {message} at {addr}")
-    logger.info(f"Received {message}")
+        message = data.decode()
+        logger.info(f"Received {message}")
 
-    # Parse the message. If we have a client 
-    request = Request(message, time.time())
+        # Parse the message. If we have a client 
+        request = Request(message, time.time())
 
-    # if reqest is valid, process, otherwise trap
-    if not request.is_valid():
-        print(f'\nnot request.is_valid()\n')
-        # TODO Raise Error
-    else:
-        pass
+        # if reqest is valid, process, otherwise trap
+        if not request.is_valid():
+            pass
+            # TODO Raise Error
+        else:
+            pass
 
-    is_new, rec = get_or_create_client_record(request)
-    payload = None
-    flood = False
-    if not request.is_valid():
-        print(f'\nnot request.is_valid()\n')
-    else:
-        # Flood throught network
-        if request.is_at():
-            # if is_new:
-            #     # print(f'NEW from {request.sender} {str(rec.position)}')
-            #     records[rec.addr] = rec
-            #     flood = True
-            if is_new or rec.client_time != request.client_time:
-                print(f'NEW from {request.sender} {str(rec.position)}')
-                records[rec.addr] = rec
-                flood = True
-            # elif str(rec.position) != Position.coords(request.lat, request.lon):
-            #     # print(f'UPDATE {str(rec.position)=} to {Position.coords(request.lat, request.lon)=}')
-            #     records[rec.addr] =  make_record(request)
-            #     flood = True
-            # else:
-            #     # print(f'END PROP of {str(request)} at {str(rec.position)}')
-            #     pass
-
-        # Do stuff with the record
-        elif is_new:
-            # Invalid Request by new Client
-            if request.is_whatisat():
-                # Need a location before we can answer whatisat
-                request.mark_invalid()
-
-            # New Client
-            elif request.is_iamat(): 
-                records[rec.addr] = rec
-                flood = True
-        
-        # Update an existing Client
-        else:    
-            if request.is_iamat():
-                # if iamat and location is same, reply to client only
-                if str(rec.position) != Position.coords(request.lat, request.lon):
-                    # FIXME I don't like how this update is occuring. just make direct like the rest of the code
-                    rec = update_record(rec, request)
-
+        is_new, rec = get_or_create_client_record(request)
+        payload = None
+        flood = False
+        if not request.is_valid():
+            pass
+        else:
+            # Flood throught network
+            if request.is_at():
+                if is_new or rec.client_time != request.client_time:
+                    records[rec.addr] = rec
                     flood = True
 
-            # Existing Client Query
-            elif request.is_whatisat():
-                if rec.position.radius == request.radius:
-                    # serve a subset of previously queried data
-                    if request.pagination <= rec.position.pagination:
-                        # construct client response with payload
-                        payload = json.loads(rec.position.payload)
-                        payload['results'] = payload['results'][:request.pagination]
-                        payload = json.dumps(payload)
+            # Do stuff with the record
+            elif is_new:
+                # Invalid Request by new Client
+                if request.is_whatisat():
+                    # Need a location before we can answer whatisat
+                    request.mark_invalid()
 
-                    # Do an API call to get more results
-                    elif rec.position.pagination <= request.pagination:
-                        loc = request.api_location
+                # New Client
+                elif request.is_iamat(): 
+                    records[rec.addr] = rec
+                    flood = True
+            
+            # Update an existing Client
+            else:    
+                if request.is_iamat():
+                    # if iamat and location is same, reply to client only
+                    rec = update_record(rec, request)
+                    flood = True
+
+                # Existing Client Query
+                elif request.is_whatisat():
+                    if rec.position.radius == request.radius:
+                        # serve a subset of previously queried data
+                        if request.pagination <= rec.position.pagination:
+                            # construct client response with payload
+                            payload = json.loads(rec.position.payload)
+                            payload['results'] = payload['results'][:request.pagination]
+                            payload = json.dumps(payload)
+
+                        # Do an API call to get more results
+                        elif rec.position.pagination <= request.pagination:
+                            loc = request.api_location
+                            rad = request.radius
+                            pag = request.pagination
+                            logger.debug(f'{request=}')
+                            api_response = await api_call(loc, rad, pag)
+                            
+                            # update record with new pagesize and payload
+                            rec.position.pagination = request.pagination
+                            rec.position.payload = json.dumps(api_response)
+                            payload = rec.position.payload
+                        # Invalid response
+                        else:
+                            request.mark_invalid()
+                            raise Exception('Received an invalid exception')
+
+                    else:
+                        # perform api query
+                        # api_response = await api_call(rec.position, rec.position.radius)
+                        # with open('places_raw.json', 'w') as f:
+                        #     json.dump(api_response, f)
+
+                        # FIXME: location hack. fix this
+                        loc = rec.position.api_location
                         rad = request.radius
                         pag = request.pagination
-                        logger.debug(f'{request=}')
+                        logger.debug(f'{str(request)=}')
                         api_response = await api_call(loc, rad, pag)
-                        
-                        # update record with new pagesize and payload
-                        rec.position.pagination = request.pagination
+
+                        # update record
+                        # print("updating record")
+                        rec.position.radius = rad
+                        rec.position.pagination = pag
                         rec.position.payload = json.dumps(api_response)
                         payload = rec.position.payload
-                    # Invalid response
-                    else:
-                        request.mark_invalid()
-                        raise Exception('Received an invalid exception')
-
                 else:
-                    # perform api query
-                    # api_response = await api_call(rec.position, rec.position.radius)
-                    # with open('places_raw.json', 'w') as f:
-                    #     json.dump(api_response, f)
+                    # TODO: Do I need to do something here?
+                    pass
+            
+        # Respond to Client
+        if not request.is_at():
+            resp = request.response(MYNAME, rec, payload=payload)
+            
+            # Reply to sender
+            logger.info(f"Send: {resp}")
+            writer.write(resp.encode())
+            await writer.drain()
 
-                    # FIXME: location hack. fix this
-                    loc = rec.position.api_location
-                    rad = request.radius
-                    pag = request.pagination
-                    logger.debug(f'{str(request)=}')
-                    api_response = await api_call(loc, rad, pag)
+            logger.debug("Close the connection")
+            writer.close()
+            await writer.wait_closed()
 
-                    # update record
-                    # print("updating record")
-                    rec.position.radius = rad
-                    rec.position.pagination = pag
-                    rec.position.payload = json.dumps(api_response)
-                    payload = rec.position.payload
-            else:
-                # TODO: Do I need to do something here?
-                pass
-        
-    # Respond to Client
-    if not request.is_at():
-        print(f'\nrequest.is_valid and not request.is_at(). {request.type=}\n')
-        resp = request.response(MYNAME, rec, payload=payload)
-        
-        # Reply to sender
-        logger.info(f"Send: {resp}")
-        writer.write(resp.encode())
-        await writer.drain()
-
-        logger.debug("Close the connection")
-        writer.close()
-        await writer.wait_closed()
-
-    # Propigate to neighbors
-    if request.is_valid():
-        if request.is_at() or flood:
-            await propagate(request)
+        # Propigate to neighbors
+        if request.is_valid():
+            if request.is_at() or flood:
+                await propagate(request)
         
 
 async def propagate(request: Request):
     request.mark_visited(MYNAME)
     visited = request.get_visited()
     to_visit = [x for x in graph[MYNAME] if x not in visited]
-    # to_visit = graph[MYNAME]
     resp = request.flood_response(MYNAME)
 
     for neighbor in to_visit:
@@ -331,18 +300,11 @@ async def places_api_call(location, radius, pag):
         f'&radius={radius}' +
         f'&key={key}'
     )
-    # logger.debug(f"api_call({location}, {radius}, {pag}) {url=}")
-    # url = (
-    #     # f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522%2C151.1957362&radius=50&type=restaurant&keyword=cruise&key={key}"
-    #     f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522%2C151.1957362&radius=50&key={key}"
-    # )
     ret = None
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as resp:
             data = await resp.text()
-            # print(f'{data=}')
             json_data = json.loads(data)
-            # print(f'{json_data=}')/
             if len(results := json_data['results']) >= pag:
                 results = results[:pag]
 
